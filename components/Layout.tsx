6c1ebf5fd31ee6544d7f9fb1bda76019aff7cbb1
choidy180 <choidy180@naver.com>
2023-01-09 00:26:22 +0900

    'commit'

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import Header from './Header';
+import { Container } from '@chakra-ui/react';
+import Footer from './Footer';
+
+type Props = {
+    children: React.ReactNode;
+}
+
+export default function Layout(props: Props){
+    return (
+        <>
+            <Container maxW="container.xl" p={0}>
+                <Header/>
+                {props.children}
+                <Footer/>
+            </Container>
+        </>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import { SessionProvider } from 'next-auth/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import Layout from '../components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <title>Kakao Naver Login NextAuthJS Prisma App</title>
+        </Head>
+        <SessionProvider session={pageProps.session}>
+          <ChakraProvider resetCSS={true}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ChakraProvider>
+        </SessionProvider>
+      </>
+  )
 }
